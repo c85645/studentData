27304f5dc7ba8a7a3e47054095ea201443e23afd
Yuan <c85645@gmail.com>
2017-11-18 17:03:39 +0800

    20171118 新增帳號管理與子選項三個功能的假畫面

--- resources/views/layout/admin/slider.blade.php
+++ resources/views/layout/admin/slider.blade.php
@@ -1,43 +1,18 @@
 <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
   <ul class="nav nav-pills flex-column">
     <li class="nav-item">
-      <a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
+      <a class="nav-link" href="#">學制管理</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Reports</a>
+      <a class="nav-link" href="#">學生資料管理</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Analytics</a>
+    <li data-toggle="collapse" data-target="#products" class="nav-item collapsed active">
+      <a class="nav-link" href="#">帳號管理</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Export</a>
-    </li>
-  </ul>
-
-  <ul class="nav nav-pills flex-column">
-    <li class="nav-item">
-      <a class="nav-link" href="#">Nav item</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Nav item again</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">One more nav</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Another nav item</a>
-    </li>
-  </ul>
-
-  <ul class="nav nav-pills flex-column">
-    <li class="nav-item">
-      <a class="nav-link" href="#">Nav item again</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">One more nav</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Another nav item</a>
-    </li>
+    <ul class="sub-menu collapse" id="products">
+        <li><a href="/admin/userSetting">使用者設定</a></li>
+        <li><a href="/admin/roleSetting">角色設定</a></li>
+        <li><a href="/admin/authSetting">權限設定</a></li>
+    </ul>
   </ul>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,33 @@
 |
 */
 
+// 登入頁面
 Route::get('/', function () {
+    return view('auth.login');
+});
+
+// 後台首頁
+Route::get('/admin', function () {
     return view('admin.index');
+});
+
+// 學制管理 Prototype
+
+
+// 學生資料管理 Prototype
+
+
+// 使用者設定 Prototype
+Route::get('/admin/userSetting', function () {
+    return view('admin.userSetting');
+});
+
+// 角色設定 Prototype
+Route::get('/admin/roleSetting', function () {
+    return view('admin.roleSetting');
+});
+
+// 權限設定 Prototype
+Route::get('/admin/authSetting', function () {
+    return view('admin.authSetting');
 });

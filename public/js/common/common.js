d4644a8cefbed9f11955b99f17475c651d8e6986
Yuan <c85645@gmail.com>
2017-11-27 11:58:48 +0800

    補上ajax方法

--- public/js/common/common.js
+++ public/js/common/common.js
@@ -70,3 +70,116 @@ function toDigitalEnglishUpper(obj) {
   var val = obj.value.toUpperCase();
   obj.value = val.replace(/[^A-Z0-9]/g, '');
 }
+
+/**
+ * jquery ajax common fucntion
+ *
+ * @param url
+ *            url網址
+ * @param param
+ *            post 傳入至action參數
+ * @param callback
+ *            success function name
+ * @param errorCallback
+ *            error function name
+ */
+function ajaxRequest(url, param, callback, errorCallback) {
+  $.ajax({
+    contentType : "application/x-www-form-urlencoded; charset=utf-8",
+    type : 'post',
+    url : url,
+    data : param,
+    async : false,
+    dataType : "json",
+    success : function(data) {
+      callback(data);
+    },
+    error : function(data) {
+      errorCallback(data);
+    }
+  });
+}
+
+/**
+ * jquery ajax common fucntion
+ *
+ * @param url
+ *            URL網址
+ * @param param
+ *            傳入至action參數
+ * @param callback
+ *            成功的方法
+ * @param errorCallback
+ *            失敗的方法
+ */
+function ajaxRequestBlockUI(url, param, callback, errorCallback) {
+  $.ajax({
+    contentType : "application/x-www-form-urlencoded; charset=utf-8",
+    type : 'post',
+    url : url,
+    data : param,
+    async : true,
+    cache : false,
+    dataType : "json",
+    /** 執行function前先鎖定畫面 */
+    beforeSend : function() {
+      $.fn.blockUI({
+        message : $("#domMessage").html($("#domMessage").html()),
+        css : {
+          border : '1px solid',
+          width : '200px',
+          top : '40%',
+          left : '40%'
+        }
+      });
+    },
+    /** 執行結束後解鎖畫面 */
+    complete : function() {
+      $.fn.unblockUI();
+    },
+    success : function(data) {
+      callback(data);
+    },
+    error : function(data) {
+      errorCallback(data);
+    },
+  });
+}
+
+/**
+ * 將頁面的textbox 指定為readonly ,checkbox、radio、select 指定為 disabled
+ */
+function allReadOnly() {
+  $(":input").each(function() {
+    if ($(this).attr("type") == "text") {
+      $(this).attr("readonly", "readonly");
+    }
+    if ($(this).attr("type") == "checkbox") {
+      $(this).attr("disabled", "disabled");
+    }
+    if ($(this).attr("type") == "radio") {
+      $(this).attr("disabled", "disabled");
+    }
+  });
+
+  $("select").each(function() {
+    $(this).attr("disabled", "disabled");
+  });
+}
+function cleanAllValue() {
+  $(":input").each(function() {
+    if ($(this).attr("type") == "text") {
+      $(this).val("");
+    }
+    if ($(this).attr("type") == "checkbox") {
+      $(this).removeAttr("checked");
+    }
+    if ($(this).attr("type") == "radio") {
+      $(this).removeAttr("checked");
+    }
+  });
+
+  $("select").each(function() {
+    $(this).get(0).selectedIndex = 0;
+  });
+}

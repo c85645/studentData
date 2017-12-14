e82c08cca669f36bdbf1d501e29ac6070b96feec
Yuan <c85645@gmail.com>
2017-12-14 23:13:43 +0800

    修改欄位順序

--- database/migrations/2017_12_02_122606_create_scores_table.php
+++ database/migrations/2017_12_02_122606_create_scores_table.php
@@ -14,9 +14,9 @@ class CreateScoresTable extends Migration
     public function up()
     {
         Schema::create('scores', function (Blueprint $table) {
+            $table->integer('academy_id');
             $table->integer('student_id');
             $table->integer('teacher_id');
-            $table->integer('academy_id');
             $table->integer('step');->default(1);
             $table->integer('no');
             $table->decimal('score', 5, 0)->nullable();

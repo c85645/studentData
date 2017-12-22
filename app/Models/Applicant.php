589a16e192b232a150600d707d5788813f240ffd
Yuan <c85645@gmail.com>
2017-12-22 17:26:54 +0800

    刪除多的設定

--- app/Models/Applicant.php
+++ app/Models/Applicant.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Applicant extends Model
 {
-    protected $guarded =[];
-
     protected $fillable = ['academy_id', 'name', 'personal_id',
         'mobile', 'email', 'pdf_path', 'transfer_grade', 'upload_time'];
 }

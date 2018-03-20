6dbe05f0844fb5922d32ab8892e437b47322d335
Yuan <c85645@gmail.com>
2018-03-20 21:37:55 +0800

    註解設定語系

--- app/Http/Controllers/GradeManageController.php
+++ app/Http/Controllers/GradeManageController.php
@@ -54,7 +54,7 @@ class GradeManageController extends Controller
             ->get();
 
             // 設定語系
-            CarbonInterval::setLocale('zh_tw');
+            // CarbonInterval::setLocale('en');
             // 這裡要判斷，1.時間未到/超過則回傳 未開放評分 2.在評分期間則回傳 還有多少天數與小時
             foreach ($academies as $academy) {
                 $score_sdate = Carbon::createFromFormat('Y-m-d', $academy->score_sdate)->startOfDay();

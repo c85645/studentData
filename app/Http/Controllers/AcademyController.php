8333c93a9b0e9ef972d3cc9d9cb3f1265e5f0a4d
Yuan <c85645@gmail.com>
2018-06-11 17:22:02 +0800

    Issue 15 管理員失去權限Fix

--- app/Http/Controllers/AcademyController.php
+++ app/Http/Controllers/AcademyController.php
@@ -80,8 +80,16 @@ class AcademyController extends Controller
         // 填寫內容更新至學制
         $academy->update(request()->only(['fill_out_sdate', 'fill_out_edate',
             'score_sdate', 'score_edate', 'pdf_url']));
+        $teacherArray;
+        if (request('owners')) {
+            $teacherArray = request('owners');
+            array_push($teacherArray, (string)(auth()->user()->id));
+        } else {
+            $teacherArray = array((string)(auth()->user()->id));
+        }
+
         // 更新負責的老師
-        $academy->teachers()->sync(request('owners'));
+        $academy->teachers()->sync($teacherArray);
 
         // 處理評分項目
         $data = request()->all();

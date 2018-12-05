5d1c40d0a8a500948e0e52cc534a93b9fa59b89f
andy86715 <andy86715@gmail.com>
2018-12-05 21:07:24 +0800

    修改GA程式碼放置位置

--- resources/views/layout/web/script.blade.php
+++ resources/views/layout/web/script.blade.php
@@ -14,5 +14,16 @@
 <script src="{{ asset('js/web/main.js') }}"></script>
 <!-- Modernizr JS -->
 <script src="{{ asset('js/web/modernizr-2.6.2.min.js') }}"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-64640280-4"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-64640280-4', 'auto');
+  ga('send', 'pageview');
+</script>
 
 
--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -3,17 +3,6 @@
 @section('html')
 <!-- Counter -->
 <script src="{{ asset('js/web/counter.js') }}"></script>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-64640280-4"></script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-64640280-4', 'auto');
-  ga('send', 'pageview');
-</script>
 
 <nav class="gtco-nav" role="navigation">
   <div class="container">
